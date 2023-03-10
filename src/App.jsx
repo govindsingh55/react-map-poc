0b31c88c4641fd22077fc6d02068b9859900a575
govindsingh <govindsingh3091997@gmail.com>
2023-03-10 14:42:15 +0530

    autocomplet address with cutom marker icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,56 @@
 import { useState } from 'react';
-import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';;
+import { GoogleMap, Marker, useLoadScript, Autocomplete } from '@react-google-maps/api';
 // lat: 26.837730 long: 83.710610
+
+const DEFAULT_MAP_CENTER = { lat: 26.837730, lng: 83.710610 };
 function Map() {
-  const { isLoaded, loadError } = useJsApiLoader({
-    id: 'google-map-script',
+  const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: import.meta.env.VITE_MAP_API_KEY,
+    libraries: ['places']
   });
-
-  const [center, setCenter] = useState({ lat: 20.5937, lng: 78.9629 });
+  const [mapCenter, setMapCenter] = useState(DEFAULT_MAP_CENTER);
   const [markers, setMarkers] = useState([{ lat: 28.7041, lng: 77.1025 }]);
-  const [latInput, setLatInput] = useState('');
-  const [lngInput, setLngInput] = useState('');
-
-  const handleLatInputChange = (event) => {
-    setLatInput(event.target.value);
-  }
-
-  const handleLngInputChange = (event) => {
-    setLngInput(event.target.value);
-  }
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    const newMarker = { lat: parseFloat(latInput), lng: parseFloat(lngInput) };
-    setMarker(newMarker);
-    setCenter(newMarker);
-    setLatInput('');
-    setLngInput('');
-  }
-
+  const [autocomplete, setAutocomplete] = useState(null);
 
   const handleMapClick = (event) => {
     const newMarker = { lat: event.latLng.lat(), lng: event.latLng.lng() };
     setMarkers([...markers, newMarker]);
   }
 
+  const handlePlaceSelect = (place) => {
+    const lat = place.geometry.location.lat();
+    const lng = place.geometry.location.lng();
+    setMapCenter({ lat, lng });
+  };
+
   if (loadError) return <div>Map cannot be loaded right now, sorry.</div>
 
   return isLoaded ? (
     <div style={{ height: '100vh', width: '100%' }}>
-      <form onSubmit={handleFormSubmit}>
-        <label htmlFor="lat-input">Latitude: </label>
-        <input id="lat-input" type="text" value={latInput} onChange={handleLatInputChange} className="border border-gray-500 rounded-sm my-1" />
-        <br />
-        <label htmlFor="lng-input">Longitude: </label>
-        <input id="lng-input" type="text" value={lngInput} onChange={handleLngInputChange} className="border border-gray-500 rounded-sm my-1" />
-        <br />
-        <button type="submit" className="m-auto bg-indigo-500 text-white px-2 py-1 rounded-sm">Update</button>
-      </form>
+      <Autocomplete
+        onLoad={(autocomplete) => {
+          console.log('auto complete : ', autocomplete)
+          setAutocomplete(autocomplete)
+        }}
+        onPlaceChanged={() => {
+          const place = autocomplete.getPlace();
+          handlePlaceSelect(place);
+        }}
+      >
+        <input placeholder="Enter an address" />
+      </Autocomplete>
       <GoogleMap
         mapContainerStyle={{ height: '100%', width: '100%' }}
-        center={center}
-        zoom={5}
+        center={mapCenter}
+        zoom={8}
         onClick={handleMapClick}
       >
         {markers.map(marker => (
           <Marker
+            key={`${marker.lat}-${marker.lng}`}
             position={marker}
             icon={{
-              url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+              url: 'education-marker.png',
               scaledSize: new window.google.maps.Size(50, 50),
               origin: new window.google.maps.Point(0, 0),
               anchor: new window.google.maps.Point(25, 50)
